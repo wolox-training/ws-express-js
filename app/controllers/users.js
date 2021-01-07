3f2dc09d8e23387377ddfe571b2ce583d36231ed
William <srwilliamg@gmail.com>
2021-01-07 13:54:41 -0500

    Added sign-up endpoint and validations

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -6,15 +6,18 @@ const WOLOX_EMAIL_DOMAIN = '@wolox';
 const UsersController = module.exports;
 
 UsersController.createUser = async (req, res) => {
-  const { body, body: { email } = {} } = req;
+  const { body, body: { name, email, password, lastName } = {} } = req;
 
-  if (!email.includes(WOLOX_EMAIL_DOMAIN)) throw errors.badRequestError('Not valid email');
+  if (!name) throw errors.badRequestError('Not valid name');
+  if (!lastName) throw errors.badRequestError('Not valid last name');
+  if (!password || password.length < 8) throw errors.badRequestError('Not valid password');
+  if (!email || !email.includes(WOLOX_EMAIL_DOMAIN)) throw errors.badRequestError('Not valid email');
 
   const createdUser = await UsersServices.createUser(body);
 
-  const { name } = createdUser;
+  const { name: userName } = createdUser;
 
   logger.info('User created successfully');
 
-  res.status(201).send({ name });
+  res.status(201).send({ name: userName });
 };
